12aa7087228daf6361557beb26f8bd9c4cc742f2
Yaroslav Bugaria <yrs.raider@gmail.com>
2021-04-25 10:30:39 -0700

    Fix get-version.ps1

--- get-version.ps1
+++ get-version.ps1
@@ -5,7 +5,7 @@ if (-not $?) {
 
 $Version = [Version]::Parse($Tag.TrimStart('v'))
 if ($Version.Build -lt 0 -and $env:GITHUB_RUN_NUMBER) {
-    $Version.Build = [Version]::new($Version.Major, [Math]::Max($Version.Minor, 0), $env:GITHUB_RUN_NUMBER)
+    $Version = [Version]::new($Version.Major, [Math]::Max($Version.Minor, 0), $env:GITHUB_RUN_NUMBER)
 }
 
 Write-Output "BUILD_VERSION=$Version" >> $env:GITHUB_ENV
