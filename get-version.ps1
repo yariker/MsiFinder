2a9c8d4e0dd9cfc5eac1b92107ca27f98bdb2d5a
Yaroslav Bugaria <yrs.raider@gmail.com>
2021-04-25 10:25:28 -0700

    Add get-version.ps1

---
+++ get-version.ps1
@@ -0,0 +1,12 @@
+$Tag = git describe --tags --abbrev=0 --match v[0-9]* HEAD
+if (-not $?) {
+    $Tag = "v0.0"
+}
+
+$Version = [Version]::Parse($Tag.TrimStart('v'))
+if ($Version.Build -lt 0 -and $env:GITHUB_RUN_NUMBER) {
+    $Version.Build = [Version]::new($Version.Major, [Math]::Max($Version.Minor, 0), $env:GITHUB_RUN_NUMBER)
+}
+
+Write-Output "BUILD_VERSION=$Version" >> $env:GITHUB_ENV
+$Version.ToString()
